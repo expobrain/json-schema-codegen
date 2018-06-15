c42a717ae4ef87b2a3dacf99beeafb50a3fb120c
Daniele Esposti <daniele.esposti@gmail.com>
2018-06-15 09:28:39 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="json-schema-codegen",
+    version="0.1",
+    keywords="pytohn javascript json-schema codegen",
+    author="Daniele Esposti",
+    author_email="daniele.esposti@gmail.com",
+    url="https://github.com/expobrain/json-schema-codegen",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["json_codegen = json_codegen:main"]},
+    python_requires="<=3",
+    license="MIT",
+    install_requires=["astor==0.6.2", "pathlib2==2.3.0"],
+)
