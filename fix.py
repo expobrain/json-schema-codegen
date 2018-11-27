0e85308a55f947e28f0b18704a46af1715324b4d
Akhil Nair <akhil.nair@revolut.co.uk>
2018-11-27 11:47:27 +0000

    Builds from_dict

--- fix.py
+++ fix.py
@@ -32,28 +32,62 @@ import ast
 class Fn_DictFrom(object):
     def __init__(self):
         self.name = "from_dict"
-        self.body = []
+        self.params = {"d": "dict"}
+        self.required = ["name", "type"]
+        self.built_fn_args = self._get_args()
+        self.built_fn_body = self._get_body()
+        self.built_fn = self._get_function()
 
-    def fn_args(self):
-        return ast.arguments(
-            args=[ast.arg(arg="d", annotation="dict")], vararg=None, kwarg=None, defaults=[]
+    def _get_args(self):
+        params = [ast.arg(arg=k, annotation=v) for (k, v) in self.params.items()]
+        args = ast.arguments(args=params, vararg=None, kwarg=None, defaults=[])
+        return args
+
+    def _get_body(self):
+        body = []
+        # out = Transform()
+        body.append(ast.Assign(targets=[ast.Name(id="d")], value=ast.Dict(keys=[], values=[])))
+        # if not d:
+        #    return out
+        body.append(
+            ast.If(
+                test=ast.UnaryOp(op=ast.Not(), operand=ast.Name(id="d")),
+                body=[ast.Return(value=ast.Name(id="out"))],
+                orelse=[],
+            )
         )
+        # if {required} not in d:
+        #     raise Exception("Required field {required} missing from class")
+        for required in self.required:
+            body.append(self._if_missing_required(required))
 
-    def fn_body(self):
-        return ast.Assign(targets=[ast.Name(id="data")], value=ast.Dict(keys=[], values=[]))
+        body.append(ast.Return(ast.Name(id="out")))
 
-    def fn_function(self):
-        return ast.FunctionDef(
-            name=self.name,
-            args=self.fn_args(),
-            body=[self.fn_body()],
-            returns=None,
-            decorator_list=[],
+        return body
+
+    def _get_function(self):
+        return [
+            ast.FunctionDef(
+                name=self.name,
+                args=self.built_fn_args,
+                body=self.built_fn_body,
+                returns=None,
+                decorator_list=[],
+            )
+        ]
+
+    def _if_missing_required(self, required):
+        str_exception = f"Required field {required} missing from Class"
+        exception = ast.Call(
+            func=ast.Name(id="Exception"), args=[ast.Str(s=str_exception)], keywords=[]
         )
+        test = ast.Compare(
+            left=ast.Str(s=required), ops=[ast.NotIn()], comparators=[ast.Name(id="d")]
+        )
+        return ast.If(test=test, body=[ast.Raise(exc=exception, cause=None)], orelse=[])
 
     def print(self):
-        self.body.append(self.fn_function())
-        print(astor.to_source(ast.Module(body=self.body)))
+        print(astor.to_source(ast.Module(body=self.built_fn)))
 
 
 Fn_DictFrom().print()
