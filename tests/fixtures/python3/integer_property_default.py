40330ba016a07e40d78862f00b7530c53a6ef23b
Akhil Nair <akhil.nair@revolut.co.uk>
2018-11-30 19:23:55 +0000

    Fixing test dir

--- tests/fixtures/python3/integer_property_default.py
+++ tests/fixtures/python3/integer_property_default.py
@@ -1,5 +1,21 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields as fields_, post_load
+from typing import Optional, List
 
 
 class TestSchema(Schema):
-    x = fields.Integer(default=42)
+    x = fields_.Integer(default=42)
+
+    @post_load
+    def make_test(self, test):
+        return Test(test)
+
+
+class Test(object):
+    def __init__(self, test: dict):
+        self.x: Optional[int] = test.get("x")
+
+    def to_json(self):
+        return TestSchema(strict=True).dumps(self).data
+
+    def to_dict(self):
+        return TestSchema(strict=True).dump(self).data
