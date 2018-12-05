a5125ffa5f40fbb0404b42c1b64318199c1e9362
Akhil Nair <akhil.nair@revolut.co.uk>
2018-12-05 09:55:59 +0000

    Refactor annotations into seperate class

--- json_codegen/generators/python3object.py
+++ json_codegen/generators/python3object.py
@@ -33,49 +33,49 @@ class Python3ObjectGenerator(object):
         )
 
     @staticmethod
-    def _annotation_optional(type_, optional=False):
-        if optional:
-            return ast.Subscript(
-                value=ast.Name(id="Optional"), slice=ast.Index(value=ast.Name(id=type_))
-            )
-        else:
-            return ast.Name(id=type_)
+    def _init_non_primitive_nested_class(node_assign, object_, prop):
+        """
+        If the nested list is non-primitive, initialise sub-classes in a list comp
+        If the nest is primitive, we can simply get it
+        Marshmallow will do the type marshalling
+        """
+        return ast.ListComp(
+            elt=ast.Call(
+                func=ast.Name(id=Python3ObjectGenerator._nesting_class(node_assign)),
+                args=[ast.Name(id="el")],
+                keywords=[],
+            ),
+            generators=[
+                ast.comprehension(
+                    target=ast.Name(id="el"),
+                    iter=ast.Call(
+                        func=ast.Attribute(value=ast.Name(id=object_), attr="get"),
+                        args=[ast.Str(s=prop), ast.Dict(keys=[], values=[])],
+                        keywords=[],
+                    ),
+                    ifs=[],
+                    is_async=0,
+                )
+            ],
+        )
 
     @staticmethod
-    def _annotation_list(subtype):
-        return "List[" + subtype[0] + "]"
+    def _get_key_from_object(object_, prop):
+        return ast.Call(
+            func=ast.Attribute(value=ast.Name(id=object_), attr="get"),
+            args=[ast.Str(s=prop)],
+            keywords=[],
+        )
 
     @staticmethod
-    def _type_annotation(node_assign):
-        """
-        Gets the type hint for a marshmallow field
-        """
-        # If the keyword required has been supplied, wrap the type hint
-        # in `Optional`. As the class is generated, `required=False` does
-        # not occur
-        optional = True
+    def _hint_required_property(node_assign, value, object_, prop):
         for node in ast.walk(node_assign):
-            if isinstance(node, ast.keyword) and node.arg == "required":
-                optional = False
-
-        for node in ast.walk(node_assign):
-
-            if isinstance(node, ast.Attribute) and node.value.id == "fields_":
-                # If the type is not a `List`, return either the type (primitive or custom)
-                type_annotation = python_type_map.get(
-                    node.attr, Python3ObjectGenerator.upper_first_letter(node.attr)
-                )
-                if type_annotation != "List":
-                    return Python3ObjectGenerator._annotation_optional(type_annotation, optional)
-
-            if isinstance(node, ast.Call) and node.func.attr == "Nested":
-                # If the type is a List, wrap the subtype with `List`
-                subtype = [Python3ObjectGenerator.class_name(n.id) for n in node.args]
-                if len(subtype) != 1:
-                    raise ValueError("Nested Schema called with more than 1 type")
-                return Python3ObjectGenerator._annotation_optional(
-                    Python3ObjectGenerator._annotation_list(subtype), optional
-                )
+            if isinstance(node, ast.keyword):
+                if "required" in node.arg:
+                    value = ast.Subscript(
+                        value=ast.Name(id=object_), slice=ast.Index(value=ast.Str(s=prop))
+                    )
+        return value
 
     @staticmethod
     def assign_property(node_assign, object_):
@@ -88,50 +88,23 @@ class Python3ObjectGenerator(object):
         prop = Python3ObjectGenerator._get_property_name(node_assign)
 
         if Python3ObjectGenerator._non_primitive_nested_list(node_assign):
-            # If the nested list is non-primitive, initialise sub-classes in a list comp
-            # If the nest is primitive, we can simply get it
-            # Marshmallow will do the type marshalling
-            value = ast.ListComp(
-                elt=ast.Call(
-                    func=ast.Name(id=Python3ObjectGenerator._nesting_class(node_assign)),
-                    args=[ast.Name(id="el")],
-                    keywords=[],
-                ),
-                generators=[
-                    ast.comprehension(
-                        target=ast.Name(id="el"),
-                        iter=ast.Call(
-                            func=ast.Attribute(value=ast.Name(id=object_), attr="get"),
-                            args=[ast.Str(s=prop), ast.Dict(keys=[], values=[])],
-                            keywords=[],
-                        ),
-                        ifs=[],
-                        is_async=0,
-                    )
-                ],
+            value = Python3ObjectGenerator._init_non_primitive_nested_class(
+                node_assign, object_, prop
             )
-
         else:
             # Assign the property as self.prop = table.get("prop")
-            value = ast.Call(
-                func=ast.Attribute(value=ast.Name(id=object_), attr="get"),
-                args=[ast.Str(s=prop)],
-                keywords=[],
-            )
+            value = Python3ObjectGenerator._get_key_from_object(object_, prop)
+
             # If the property is required, assign as self.prop = table["prop"]
-            for node in ast.walk(node_assign):
-                if isinstance(node, ast.keyword):
-                    if "required" in node.arg:
-                        value = ast.Subscript(
-                            value=ast.Name(id=object_), slice=ast.Index(value=ast.Str(s=prop))
-                        )
+            value = Python3ObjectGenerator._hint_required_property(
+                node_assign, value, object_, prop
+            )
 
         return ast.AnnAssign(
             target=ast.Attribute(value=ast.Name(id="self"), attr=prop),
             value=value,
             simple=0,
-            # annotation=ast.Name(id=Python3ObjectGenerator._type_annotation(node_assign)),
-            annotation=Python3ObjectGenerator._type_annotation(node_assign),
+            annotation=Annotations(node_assign).type,
         )
 
     @staticmethod
@@ -285,3 +258,50 @@ class Python3ObjectGenerator(object):
         """
         return s[0].upper() + s[1:]
 
+
+class Annotations:
+    def __init__(self, node):
+        self.node = node
+        self.type = self.type_annotation()
+
+    def type_annotation(self):
+        """
+        Gets the type hint for a marshmallow field
+        """
+        # If the keyword required has been supplied, wrap the type hint
+        # in `Optional`. As the class is generated, `required=False` does
+        # not occur
+        for node in ast.walk(self.node):
+            if isinstance(node, ast.keyword) and node.arg == "required":
+                optional = False
+                break
+        else:
+            optional = True
+
+        for node in ast.walk(self.node):
+
+            if isinstance(node, ast.Attribute) and node.value.id == "fields_":
+                # If the type is not a `List`, return either the type (primitive or custom)
+                type_annotation = python_type_map.get(
+                    node.attr, Python3ObjectGenerator.upper_first_letter(node.attr)
+                )
+                if type_annotation != "List":
+                    return self._annotation_optional(type_annotation, optional)
+
+            if isinstance(node, ast.Call) and node.func.attr == "Nested":
+                # If the type is a List, wrap the subtype with `List`
+                subtype = [Python3ObjectGenerator.class_name(n.id) for n in node.args]
+                if len(subtype) != 1:
+                    raise ValueError("Nested Schema called with more than 1 type")
+                return self._annotation_optional(self._annotation_list(subtype), optional)
+
+    def _annotation_optional(self, type_, optional=False):
+        if optional:
+            return ast.Subscript(
+                value=ast.Name(id="Optional"), slice=ast.Index(value=ast.Name(id=type_))
+            )
+        else:
+            return ast.Name(id=type_)
+
+    def _annotation_list(self, subtype):
+        return "List[" + subtype[0] + "]"
