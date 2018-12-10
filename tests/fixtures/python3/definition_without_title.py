c5900808cb90225e0bd73d70d2b50e4c858c698a
Akhil Nair <akhil.nair@revolut.co.uk>
2018-12-10 08:32:58 +0000

    Tests pass aside from those that raise NotImplementedExeceptions

---
+++ tests/fixtures/python3/definition_without_title.py
@@ -0,0 +1,23 @@
+from marshmallow import Schema, fields as fields_, post_load
+from typing import Optional, List, Any
+
+
+class NoTitleSchema(Schema):
+    x = fields_.String()
+
+
+class NoTitle(object):
+
+    def __init__(self, notitle: dict):
+        self.x: Optional[str] = notitle.get('x')
+
+    def to_json(self):
+        return NoTitleSchema(strict=True).dumps(self).data
+
+    def to_dict(self):
+        return NoTitleSchema(strict=True).dump(self).data
+
+    @staticmethod
+    def from_json(json: str, only=None):
+        return NoTitleSchema(strict=True, only=only).loads(json).data
+
