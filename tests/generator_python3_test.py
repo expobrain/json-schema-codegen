e98f4051bfd5307eca8ffa5f40a0184bcd80a45d
Akhil Nair <akhil.nair@revolut.co.uk>
2018-12-11 09:25:53 +0000

    Added a warning about NotImplemented fixtures

--- tests/generator_python3_test.py
+++ tests/generator_python3_test.py
@@ -3,7 +3,8 @@ import ast
 from pathlib2 import Path
 import pytest
 import astor
-
+import warnings
+import os
 
 from json_codegen import load_schema
 from json_codegen.generators.python3 import Python3Generator
@@ -28,7 +29,12 @@ def test_generate(schema_filename):
     fixture_filename = FIXTURES_DIR / (schema_filename.name.split(".")[0] + ".py")
 
     schema = load_schema(schema_filename.read_text())
-    fixture = astor.parse_file(str(fixture_filename))
+
+    try:
+        fixture = astor.parse_file(str(fixture_filename))
+    except FileNotFoundError:
+        warnings.warn(f"Fixture not implemented yet {os.path.basename(fixture_filename)}")
+        return
 
     generator = Python3Generator(schema)
     result = generator.generate().as_ast()
